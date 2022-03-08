25640fcffc863ceb00ab62113f682ac2205880d9
chris9797 <christends97@gmail.com>
2022-03-08 13:37:33 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -2,3 +2,14 @@
 
 
 print("adding all ML releated code here")
+
+
+
+def model1():
+    print("adding code for model1")
+    
+def model2():
+    print("adding code for model2")
+    
+def model3():
+    print("adding code for model3")
